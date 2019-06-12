8185fbc93b906c01eb31da9cff76d170d37d2849
Wouter Roos <wrmmroos@outlook.com>
2019-06-12 15:46:42 +0200

    added missing project types

--- MvsSln/Core/Guids.cs
+++ MvsSln/Core/Guids.cs
@@ -1,18 +1,18 @@
 ﻿/*
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2013-2019  Denis Kuzmin < entry.reg@gmail.com > GitHub/3F
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,7 +22,6 @@
  * THE SOFTWARE.
 */
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -35,14 +34,16 @@ namespace net.r_eg.MvsSln.Core
         /// </summary>
         public const string SLN_FOLDER = "{2150E333-8FDC-42A3-9474-1A3956D46DE8}";
 
-        public const string PROJECT_CS  = "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}";
-        public const string PROJECT_DB  = "{C8D11400-126E-41CD-887F-60BD40844F9E}";
-        public const string PROJECT_FS  = "{F2A71F9B-5D33-465A-A702-920D77279786}";
-        public const string PROJECT_VB  = "{F184B08F-C81C-45F6-A57F-5ABD9991F28F}";
-        public const string PROJECT_VC  = "{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}";
-        public const string PROJECT_VJ  = "{E6FDF86B-F3D1-11D4-8576-0002A516ECE8}";
-        public const string PROJECT_WD  = "{2CFEAB61-6A3B-4EB8-B523-560B4BEEF521}";
+        public const string PROJECT_CS = "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}";
+        public const string PROJECT_DB = "{C8D11400-126E-41CD-887F-60BD40844F9E}";
+        public const string PROJECT_FS = "{F2A71F9B-5D33-465A-A702-920D77279786}";
+        public const string PROJECT_VB = "{F184B08F-C81C-45F6-A57F-5ABD9991F28F}";
+        public const string PROJECT_VC = "{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}";
+        public const string PROJECT_VJ = "{E6FDF86B-F3D1-11D4-8576-0002A516ECE8}";
+        public const string PROJECT_WD = "{2CFEAB61-6A3B-4EB8-B523-560B4BEEF521}";
         public const string PROJECT_WEB = "{E24C65DC-7377-472B-9ABA-BC803B73C61A}";
+        public const string PROJECT_DEPLOY = "{151D2E53-A2C4-4D7D-83FE-D05416EBD58E}";
+        public const string PROJECT_SF = "{A07B5EB6-E848-4116-A8D0-A826331D98C6}";
 
         private static Dictionary<ProjectType, string> ProjectTypeGuids = new Dictionary<ProjectType, string>()
         {
@@ -55,6 +56,8 @@ namespace net.r_eg.MvsSln.Core
             { ProjectType.Wd, PROJECT_WD },
             { ProjectType.Web, PROJECT_WEB },
             { ProjectType.SlnFolder, Guids.SLN_FOLDER },
+            { ProjectType.Deploy, PROJECT_DEPLOY },
+            { ProjectType.Sf, PROJECT_SF },
             { ProjectType.Unknown, null }
         };
 
