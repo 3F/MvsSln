802c91e100d50b470b58ce7d1869c5c59e3e778e
Wouter Roos <wouterroos@users.noreply.github.com>
2019-06-13 15:06:20 +0200

    Add deploy flag support (#16)
    * added support for deploy flags
    
    * fixed formatting
    
    * fixed indents
    
    * fixed indents (again)
    
    * reformat
    
    * reformat using vscode

--- MvsSln/Core/ConfigPrj.cs
+++ MvsSln/Core/ConfigPrj.cs
@@ -1,18 +1,18 @@
 ﻿/*
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2013-2019  Denis Kuzmin < entry.reg@gmail.com > GitHub/3F
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -52,6 +52,16 @@ namespace net.r_eg.MvsSln.Core
         }
 
         /// <summary>
+        /// Existence of `.Deploy.0` to activate project for deployment:
+        /// {A7BF1F9C-F18D-423E-9354-859DC3CFAFD4}.CI_Release|Any CPU.Deploy.0 = Release|Any CPU
+        /// </summary>
+        public bool IncludeInDeploy
+        {
+            get;
+            internal set;
+        }
+
+        /// <summary>
         /// Link to solution configuration.
         /// </summary>
         public IConfPlatform Sln
@@ -63,19 +73,32 @@ namespace net.r_eg.MvsSln.Core
         public ConfigPrj(string name, string platform, string pGuid, bool build, ConfigSln sln)
             : base(name, platform)
         {
-            Set(pGuid, build, sln);
+            Set(pGuid, build, false, sln);
         }
 
         public ConfigPrj(string formatted, string pGuid, bool build, ConfigSln sln)
             : base(formatted)
         {
-            Set(pGuid, build, sln);
+            Set(pGuid, build, false, sln);
         }
 
-        private void Set(string pGuid, bool build, ConfigSln sln)
+        public ConfigPrj(string name, string platform, string pGuid, bool build, bool deploy, ConfigSln sln)
+            : base(name, platform)
+        {
+            Set(pGuid, build, deploy, sln);
+        }
+
+        public ConfigPrj(string formatted, string pGuid, bool build, bool deploy, ConfigSln sln)
+            : base(formatted)
+        {
+            Set(pGuid, build, deploy, sln);
+        }
+
+        private void Set(string pGuid, bool build, bool deploy, ConfigSln sln)
         {
             PGuid           = pGuid;
             IncludeInBuild  = build;
+            IncludeInDeploy = deploy;
             Sln             = sln;
         }
 
