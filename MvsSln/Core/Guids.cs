90f6ad38fae1c3d65ed3a0ec35d0a296fe06aff3
Wouter Roos <wrmmroos@outlook.com>
2019-06-13 08:31:25 +0200

    fixed lookup for unknown project types

--- MvsSln/Core/Guids.cs
+++ MvsSln/Core/Guids.cs
@@ -32,18 +32,18 @@ namespace net.r_eg.MvsSln.Core
         /// <summary>
         /// Solution Folder.
         /// </summary>
-        public const string SLN_FOLDER     = "{2150E333-8FDC-42A3-9474-1A3956D46DE8}";
+        public const string SLN_FOLDER = "{2150E333-8FDC-42A3-9474-1A3956D46DE8}";
 
-        public const string PROJECT_CS     = "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}";
-        public const string PROJECT_DB     = "{C8D11400-126E-41CD-887F-60BD40844F9E}";
-        public const string PROJECT_FS     = "{F2A71F9B-5D33-465A-A702-920D77279786}";
-        public const string PROJECT_VB     = "{F184B08F-C81C-45F6-A57F-5ABD9991F28F}";
-        public const string PROJECT_VC     = "{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}";
-        public const string PROJECT_VJ     = "{E6FDF86B-F3D1-11D4-8576-0002A516ECE8}";
-        public const string PROJECT_WD     = "{2CFEAB61-6A3B-4EB8-B523-560B4BEEF521}";
-        public const string PROJECT_WEB    = "{E24C65DC-7377-472B-9ABA-BC803B73C61A}";
+        public const string PROJECT_CS = "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}";
+        public const string PROJECT_DB = "{C8D11400-126E-41CD-887F-60BD40844F9E}";
+        public const string PROJECT_FS = "{F2A71F9B-5D33-465A-A702-920D77279786}";
+        public const string PROJECT_VB = "{F184B08F-C81C-45F6-A57F-5ABD9991F28F}";
+        public const string PROJECT_VC = "{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}";
+        public const string PROJECT_VJ = "{E6FDF86B-F3D1-11D4-8576-0002A516ECE8}";
+        public const string PROJECT_WD = "{2CFEAB61-6A3B-4EB8-B523-560B4BEEF521}";
+        public const string PROJECT_WEB = "{E24C65DC-7377-472B-9ABA-BC803B73C61A}";
         public const string PROJECT_DEPLOY = "{151D2E53-A2C4-4D7D-83FE-D05416EBD58E}";
-        public const string PROJECT_SF     = "{A07B5EB6-E848-4116-A8D0-A826331D98C6}";
+        public const string PROJECT_SF = "{A07B5EB6-E848-4116-A8D0-A826331D98C6}";
 
         private static Dictionary<ProjectType, string> ProjectTypeGuids = new Dictionary<ProjectType, string>()
         {
@@ -68,8 +68,7 @@ namespace net.r_eg.MvsSln.Core
         /// <returns></returns>
         public static ProjectType ProjectTypeBy(string guid)
         {
-            return ProjectTypeGuids.Where(p => p.Value == guid)
-                    .Select(p => p.Key).First();
+            return ProjectTypeGuids.FirstOrDefault(p => p.Value == guid).Key;
         }
 
         /// <summary>
