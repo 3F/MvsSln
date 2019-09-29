723b8bf5ff26b09f51009ba90523f97e8d586829
Denis Kuzmin [ GitHub/3F ] <entry.reg@gmail.com>
2019-09-30 00:24:51 +0300

    New SDK-based project file and build scripts.
    +netstandard2.0. Issue #11

---
+++ MvsSln/InternalsVisibleTo.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: InternalsVisibleTo
+(
+    "MvsSlnTest, PublicKey=0024000004800000940000000602000000240000525341310004000001000100ef124fee5b93f3" +
+                            "a44ac41e0385ed58b1b21e26f2eb6e3940c8f7189343846ceb0c709d0aac81baebf689bbe68c85" +
+                            "1913dbd6a1dc92cec5c8a3b9b3b51615e5c397ab917152cf010d71f0cebe69f9d0e7c4cdc4eb4a" +
+                            "06a1d6481db058ab03f450550e7b8b6f861bcc5788242c65236633a3382487904551dbac8ac3d8" +
+                            "b20188c4"
+)]
+
+[assembly: Guid("f905dfb2-2e67-4ff7-9001-924f6f2d645d")]
