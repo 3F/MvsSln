a7fdda1bc63b481e628b1ffc9c3de5f4785b7162
Denis Kuzmin (github/3F) <x-3F@outlook.com>
2021-08-23 20:21:45 +0300

    +common.props unifies TFMs
    Part of issue #48

--- MvsSln/Exceptions/UnloadException.cs
+++ MvsSln/Exceptions/UnloadException.cs
@@ -30,7 +30,7 @@ using System.Security.Permissions;
 namespace net.r_eg.MvsSln.Exceptions
 {
     [Serializable]
-    public class UnloadException<T>: CommonException
+    public class UnloadException<T>: CommonException, ISerializable
     {
         public T UnloadedInstance
         {
@@ -38,7 +38,6 @@ namespace net.r_eg.MvsSln.Exceptions
             protected set;
         }
 
-        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
         public override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             base.GetObjectData(info, context);
